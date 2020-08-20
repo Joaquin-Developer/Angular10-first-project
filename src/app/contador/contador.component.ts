0fba3de9ffe3fcfca3aacf5ed958b6e35f28a85d
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-08-20 19:39:18 -0300

    finalizo ejercicio de Contador: funciona todo y bien :)

--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -14,10 +14,22 @@ export class ContadorComponent implements OnInit {
   contador: number = 0;
   verHola: boolean = false;
   verAdios: boolean = false;
+  letraEjercicio: string = "Si el contador es mayor o igual a 15, mostramos el boton Hola, y si es menor o igual a -10, " 
+  + "mostramos el boton Adios";
+  fondo: string = "";
   
-
   ngOnInit(): void {
   }
 
 }
+/**
+function name() {
+  if()
+}
 
+var btnIncremento = document.getElementById('btnFun1');
+var btnDecremento = document.getElementById('btnFun2');
+btnIncremento.onclick = function() { name(); };
+btnDecremento.onclick = function() { name(); };....
+
+ */
