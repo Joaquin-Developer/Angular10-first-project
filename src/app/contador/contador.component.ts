46a82e8523ce1fe1f75cc2b52992f043b1e01ecc
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-08-20 18:18:15 -0300

    actualizo contador.component. html

--- src/app/contador/contador.component.ts
+++ src/app/contador/contador.component.ts
@@ -10,7 +10,7 @@ export class ContadorComponent implements OnInit {
   constructor() { }
 
   // exporto las variables:
-  
+  textoTitulo: string = "Problema: Contador - Ejercicio 1"
 
   ngOnInit(): void {
   }
