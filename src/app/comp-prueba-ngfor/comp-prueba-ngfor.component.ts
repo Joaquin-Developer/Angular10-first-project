12426aeaa547a11f8c82547422b68168b026f2eb
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-08-20 17:49:46 -0300

    creo y agrego un Array de objetos

--- src/app/comp-prueba-ngfor/comp-prueba-ngfor.component.ts
+++ src/app/comp-prueba-ngfor/comp-prueba-ngfor.component.ts
@@ -9,6 +9,13 @@ export class CompPruebaNgforComponent implements OnInit {
 
   cursos: string[] = ["HTML", "CSS", "TS", "ANGULAR"];  // este array de strings lo vamos a iterar en el html con ngFor
 
+  animales: Array<any> = [
+    {tipo: "Perro", nombre: "Tomy", edad: 2},
+    {tipo: "Perro", nombre: "Arya", edad: 1},
+    {tipo: "Gato", nombre: "Manchitas", edad: 5}
+  ];
+
+
   constructor() { }
 
   ngOnInit(): void {
